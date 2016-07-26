c9efc03b9ffac2495cf8b6f2788972e47d5e726f
Dale Whinham <daleyo@gmail.com>
2016-07-26 21:44:01 +0100

    Fix build

--- firmware/source/images.h
+++ firmware/source/images.h
@@ -3,65 +3,13 @@
 
 #include "MicroBit.h"
 
-const uint8_t UBIT_IMAGE_ARRAY_left[] = {
-                      0, 0, 1, 0, 0,
-                      0, 1, 0, 0, 0,
-                      1, 1, 1, 1, 1,
-                      0, 1, 0, 0, 0,
-                      0, 0, 1, 0, 0, };
-const uint8_t UBIT_IMAGE_ARRAY_right[] = {
-                      0, 0, 1, 0, 0,
-                      0, 0, 0, 1, 0,
-                      1, 1, 1, 1, 1,
-                      0, 0, 0, 1, 0,
-                      0, 0, 1, 0, 0, };                          
-const uint8_t UBIT_IMAGE_ARRAY_up[] = {
-                      0, 0, 1, 0, 0,
-                      0, 1, 1, 1, 0,
-                      1, 0, 1, 0, 1,
-                      0, 0, 1, 0, 0,
-                      0, 0, 1, 0, 0, };              
-const uint8_t UBIT_IMAGE_ARRAY_down[] = {
-                      0, 0, 1, 0, 0,
-                      0, 0, 1, 0, 0,
-                      1, 0, 1, 0, 1,
-                      0, 1, 1, 1, 0,
-                      0, 0, 1, 0, 0, };     
-                      
-const uint8_t UBIT_IMAGE_ARRAY_back[] = {
-                      0, 0, 0, 0, 0,
-                      0, 0, 0, 0, 0,
-                      0, 0, 1, 0, 0,
-                      0, 0, 0, 0, 0,
-                      0, 0, 0, 0, 0, };                          
-const uint8_t UBIT_IMAGE_ARRAY_front[] = {
-                      0, 1, 1, 1, 0,
-                      1, 0, 0, 0, 1,
-                      1, 0, 1, 0, 1,
-                      1, 0, 0, 0, 1,
-                      0, 1, 1, 1, 0, };    
-
-const uint8_t UBIT_IMAGE_ARRAY_tick[] = {
-                      0, 0, 0, 0, 0,
-                      0, 0, 0, 0, 1,
-                      0, 0, 0, 1, 0,
-                      1, 0, 1, 0, 0,
-                      0, 1, 0, 0, 0, };    
-
-const uint8_t UBIT_IMAGE_ARRAY_key[] = {
-                      0, 0, 0, 0, 0,
-                      0, 1, 0, 0, 0,
-                      1, 0, 1, 1, 1,
-                      0, 1, 0, 0, 1,
-                      0, 0, 0, 0, 0, };    
-                
-MicroBitImage imgup(5,5,UBIT_IMAGE_ARRAY_up);
-MicroBitImage imgdown(5,5,UBIT_IMAGE_ARRAY_down); 
-MicroBitImage imgleft(5,5,UBIT_IMAGE_ARRAY_left); 
-MicroBitImage imgright(5,5,UBIT_IMAGE_ARRAY_right);  
-MicroBitImage imgfront(5,5,UBIT_IMAGE_ARRAY_front); 
-MicroBitImage imgback(5,5,UBIT_IMAGE_ARRAY_back); 
-MicroBitImage imgtick(5,5,UBIT_IMAGE_ARRAY_tick); 
-MicroBitImage imgkey(5,5,UBIT_IMAGE_ARRAY_key); 
+extern MicroBitImage imgup;
+extern MicroBitImage imgdown;
+extern MicroBitImage imgleft;
+extern MicroBitImage imgright;
+extern MicroBitImage imgfront;
+extern MicroBitImage imgback;
+extern MicroBitImage imgtick;
+extern MicroBitImage imgkey;
 
 #endif
